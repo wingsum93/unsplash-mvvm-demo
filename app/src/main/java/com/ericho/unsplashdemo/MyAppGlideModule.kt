154b362f6563a90a4328f86876fa1943d3ec7fed
Eric Ho <wingsum.developer@gmail.com>
2018-07-06 12:25:24 +0800

    change documentation

--- app/src/main/java/com/ericho/unsplashdemo/MyAppGlideModule.kt
+++ app/src/main/java/com/ericho/unsplashdemo/MyAppGlideModule.kt
@@ -1,10 +1,43 @@
 package com.ericho.unsplashdemo
 
+import android.content.Context
+import android.util.Log
+import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.DecodeFormat
+import com.bumptech.glide.load.engine.cache.LruResourceCache
+import com.bumptech.glide.load.engine.cache.MemorySizeCalculator
 import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
 
+
+/**
+ * @see <a href=http://bumptech.github.io/glide/doc/configuration.html>The official doc</a>
+ * @author Eric Ho
+ */
 
 @GlideModule
 class MyAppGlideModule : AppGlideModule() {
 
+
+    override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
+
+
+    }
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        val calculator = MemorySizeCalculator.Builder(context)
+                .setMemoryCacheScreens(2f)
+                .build()
+        builder.setMemoryCache(LruResourceCache(calculator.memoryCacheSize.toLong()));
+        builder.setDefaultRequestOptions(
+                RequestOptions()
+                        .format(DecodeFormat.PREFER_ARGB_8888)
+                        .disallowHardwareConfig())
+
+        builder.setLogLevel(Log.DEBUG)
+
+    }
 }
